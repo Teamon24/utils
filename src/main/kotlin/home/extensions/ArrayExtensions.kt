a96572d93f5281f0ad4996e46f8b679b1cd07164
Artem.Leshchenko <teamon24@gmail.com>
2023-05-26 11:51:50 +0400

    added inline specifier

--- src/main/kotlin/home/extensions/ArrayExtensions.kt
+++ src/main/kotlin/home/extensions/ArrayExtensions.kt
@@ -1,6 +1,6 @@
 package home.extensions
 
 object ArrayExtensions {
-    @JvmStatic fun <E> Array<E>.isEmpty(onTrue: () ->Unit) = isEmpty().apply { if (this) onTrue() }
-    @JvmStatic fun <E> Array<E>.isNotEmpty(onTrue: () ->Unit) = (!isEmpty()).apply { if (this) onTrue() }
+    @JvmStatic inline fun <E> Array<E>.isEmpty(onTrue: () ->Unit) = isEmpty().apply { if (this) onTrue() }
+    @JvmStatic inline fun <E> Array<E>.isNotEmpty(onTrue: () ->Unit) = (!isEmpty()).apply { if (this) onTrue() }
 }
--- src/main/kotlin/home/extensions/MapExtensions.kt
+++ src/main/kotlin/home/extensions/MapExtensions.kt
@@ -1,8 +1,8 @@
 package home.extensions
 
 object MapExtensions {
-    @JvmStatic fun <K, V> Map<K, V>.exclude(key: K) = filter { it.key != key }
-    @JvmStatic fun <K, V> Map<K, V>.excludeAll(keys: Collection<K>) = filter { it.key !in keys }
-    @JvmStatic fun <K, V> Map<K, V>.isEmpty(onTrue: () ->Unit) = isEmpty().apply { if (this) onTrue() }
-    @JvmStatic fun <K, V> Map<K, V>.isNotEmpty(onTrue: () ->Unit) = (!isEmpty()).apply { if (this) onTrue() }
+    @JvmStatic inline fun <K, V> Map<K, V>.exclude(key: K) = filter { it.key != key }
+    @JvmStatic inline fun <K, V> Map<K, V>.excludeAll(keys: Collection<K>) = filter { it.key !in keys }
+    @JvmStatic inline fun <K, V> Map<K, V>.isEmpty(onTrue: () ->Unit) = isEmpty().apply { if (this) onTrue() }
+    @JvmStatic inline fun <K, V> Map<K, V>.isNotEmpty(onTrue: () ->Unit) = (!isEmpty()).apply { if (this) onTrue() }
 }
